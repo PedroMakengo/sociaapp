fd33f4aa0d252f149c83394aee94be1b47b78955
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-28 18:15:38 -0300

    Componente de oferta

---
+++ src/pages/components/Offer/index.tsx
@@ -0,0 +1,13 @@
+import style from "./style.module.scss";
+
+export function Offer() {
+  return (
+    <div>
+      <div className={style.container}>
+        <div>
+          <h1>Oferta</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/components/Testimonies/index.tsx
@@ -0,0 +1,11 @@
+import style from "./style.module.scss";
+
+export function Testimonies() {
+  return (
+    <section>
+      <div className={style.container}>
+        <h1>Testemunhas</h1>
+      </div>
+    </section>
+  );
+}
