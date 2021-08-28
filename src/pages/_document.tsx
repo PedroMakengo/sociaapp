dcc8b7621078e2855f009efa1e867653970dd8fa
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-28 13:43:09 -0300

    Adiciondo o header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,9 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-export class MyDocument extends Document {
+export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
