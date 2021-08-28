0aca1e03d39d848a53add89bfa14d16c805cddaf
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-28 13:34:25 -0300

    Mundando as configurações da minha estrutura next padrão

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+export class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
