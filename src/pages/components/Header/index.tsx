8118ecda7f0c9686a5a09bd6113e2f8bff871702
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-28 16:40:41 -0300

    Header terminado

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -1,7 +1,21 @@
+import style from "./style.module.scss";
+
 export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className={style.header}>
+      <div className={style.container}>
+        <img src="../../images/logo-1.jpg" alt="" />
+        <nav>
+          <a href="#" className={style.active}>
+            Home
+          </a>
+          <a href="#">Sobre Nós</a>
+          <a href="#">Testemunhos</a>
+          <a href="#">Contactos</a>
+
+          <a href="#">Baixar agora</a>
+        </nav>
+      </div>
     </header>
   );
 }
