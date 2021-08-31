21b5836f3fb945a689a8c9e2503d8204108ba111
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-31 06:26:55 -0300

    Trabalhando nos testemunhos

--- src/pages/components/Testimonies/index.tsx
+++ src/pages/components/Testimonies/index.tsx
@@ -1,9 +1,28 @@
+import { FaStore } from "react-icons/fa";
 import style from "./style.module.scss";
 
 export function Testimonies() {
   return (
     <section className={style.testimonies}>
-      <div className={style.container}>{/* <h1>Testemunhas</h1> */}</div>
+      <div className={style.container}>
+        <div></div>
+        <div className={style.slider}>
+          <h1>Testemunhos</h1>
+          <div className={style.content}>
+            <div className={style.item}>
+              <img src="../../../images/firmino.jpg" alt="Augusto Firmino" />
+              <div className={style.describe}>
+                <div className={style.avatar}>
+                  <h3>Augusto Firmino</h3>
+                  <span>
+                    <FaStore />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
