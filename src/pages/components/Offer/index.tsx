37f80f7266974b6249a239f8a52cff05cfce9435
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-31 06:53:55 -0300

    Trabalhando na parte de agente sócia

--- src/pages/components/Offer/index.tsx
+++ src/pages/components/Offer/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../Button";
 import style from "./style.module.scss";
 
 export function Offer() {
@@ -5,8 +6,15 @@ export function Offer() {
     <div className={style.bannerOferta}>
       <div className={style.container}>
         <div>
-          <h1>Oferta</h1>
+          <h1>Sócia Agente</h1>
+          <p>
+            Não tens um smartphone para efetuar as operações dentro da aplicação
+            sócia ? Não se preocupe temos para si os nossos agentes que podem
+            ajudar-te com isso
+          </p>
+          <Button title="Contactar agora" />
         </div>
+        <div></div>
       </div>
     </div>
   );
